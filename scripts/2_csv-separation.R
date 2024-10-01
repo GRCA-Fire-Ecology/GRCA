00fb11c6d10f00d4ac6d133bf8e2b7d3703d712d
GRCAFireEcology <grandcanyonfirefx@gmail.com>
2024-09-30 17:37:27 -0700

    update naming of scripts

---
+++ scripts/2_csv-separation.R
@@ -0,0 +1,7 @@
+# Created by: Alexandra Lalor
+# Email: alexandra_lalor@nps.gov, allielalor@gmail.com
+# Date Created: 2024-09-30
+# Last Edited: 2024-09-30
+#
+# Take mass FFI CSV export, separate by monitoring type
+
