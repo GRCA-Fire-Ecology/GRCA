a874a65aa70208b886df9cb6992cf849f3edfa6e
GRCAFireEcology <grandcanyonfirefx@gmail.com>
2024-11-04 19:29:07 -0700

    add Fuel Constant check

--- scripts/3_QAQC_Fuels_GRCA.R
+++ scripts/3_QAQC_Fuels_GRCA.R
@@ -35,12 +35,18 @@ data_path <- "C:/Users/alalor.NPS/Desktop/R/GRCA/data/data_qaqc/2024/"
 ################################################################################
 
 # Load in data
-FuelsCWD <- read.csv(paste0(data_path, "Surface Fuels - 1000Hr_XPT.csv"))
+Fuels1000_all <- read.csv(paste0(data_path, "Surface Fuels - 1000Hr_XPT.csv"))
 
-# Fix date column
-FuelsCWD <- FuelsCWD %>%
-  separate(Date, sep = " ", into = c("Date", "Time"))
+# Fix date column and filter out header info
+Fuels1000 <- Fuels1000_all %>%
+  separate(Date, sep = " ", into = c("Date", "Time")) %>%
+  separate(MacroPlot.Name, sep = " ", into = c("MonitoringType", "Plot"), remove = FALSE) %>%
+  filter(is.na(Visited))
 
+Fuels1000_header <- Fuels1000_all %>%
+  separate(Date, sep = " ", into = c("Date", "Time")) %>%
+  separate(MacroPlot.Name, sep = " ", into = c("MonitoringType", "Plot"), remove = FALSE) %>%
+  filter(!is.na(Visited))
 
 #### Fuel 1000 DecayClass ####
 
@@ -49,18 +55,44 @@ saved_query <- "Fuel 1000 DecayClass"
 error_message <- "Invalid decay class value"
 valid_values <- c(3,4)
 
-# Extract non-missing values from the specified column, filter out NA columns
-FuelsCWD_DecayCl <- FuelsCWD %>%
-  filter(!is.na(DecayCl))
-
-# Find indices of values that are not in the list of valid_values
-errors_DecayCl <- FuelsCWD_DecayCl %>%
+# Identify errors
+errors_DecayCl <- Fuels1000 %>%
   filter(!DecayCl %in% valid_values) %>%
   mutate("Saved Query" = saved_query) %>%
   select(c("Saved Query", "Date", "MacroPlot.Name", "DecayCl")) %>%
   mutate("Error" = paste(error_message, DecayCl)) %>%
   mutate("Fixed?" = "no","Explanation" = "","Queryer" = "") %>%
   select(!"DecayCl")
+
+
+#### Fuel 1000 FuelConst (PIAB or PIED or PIEN or PIPN or PIPO) ####
+
+# Set parameters of the QAQC test (what is the query, what is the error being targeted, what are the valid values)
+saved_query <- "Fuel 1000 FuelConst"
+error_message <- "Incorrect fuel constant entered"
+valid_values <- c("PIAB", "PIED", "PIEN", "PIPN", "PIPO")
+
+# Identify errors
+# Check that monitoring type matches with Fuel Constant
+errors_FuelConst_1 <- Fuels1000 %>%
+  filter(!MonitoringType == CWDFuConSt) %>%
+  filter(!MonitoringType == "SHOS") %>%
+  mutate("Saved Query" = saved_query) %>%
+  select(c("Saved Query", "Date", "MacroPlot.Name", "CWDFuConSt")) %>%
+  mutate("Error" = paste(error_message, CWDFuConSt)) %>%
+  mutate("Fixed?" = "no","Explanation" = "","Queryer" = "") %>%
+  select(!"CWDFuConSt")
+# Check that fuel constant is among the list of valid values
+errors_FuelConst_2 <- Fuels1000 %>%
+  filter(!CWDFuConSt %in% valid_values) %>%
+  mutate("Saved Query" = saved_query) %>%
+  select(c("Saved Query", "Date", "MacroPlot.Name", "CWDFuConSt")) %>%
+  mutate("Error" = paste(error_message, CWDFuConSt)) %>%
+  mutate("Fixed?" = "no","Explanation" = "","Queryer" = "") %>%
+  select(!"CWDFuConSt")
+
+# Merge errors
+errors_FuelConst <- rbind(errors_FuelConst_1, errors_FuelConst_2)
 
 
 
@@ -73,6 +105,6 @@ errors <- data.frame(matrix(ncol = 7, nrow = 0))
 colnames(errors) <- c("Saved Query", "Date of Read", "Macro Plot ID", "Error", "Fixed?", "Explanation", "Queryer")
 
 # Save to master error list
-errors <- rbind(errors, errors_DecayCl)
+errors <- rbind(errors, errors_DecayCl, errors_FuelConst)
 
 
